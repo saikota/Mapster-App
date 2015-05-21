e62ae44b79bbd5b748f722f956ba8204be17ecf3
unknown <kotas01@SCL-KotaS01.corp.pvt>
2015-05-20 17:37:35 -0700

    instantiated the map to show the default map object.

--- main/webapp/resources/mytheme/js/home.js
+++ main/webapp/resources/mytheme/js/home.js
@@ -1,4 +1,12 @@
-(function(window){
-	
-	 
-}(window,google));
+(function(window, google, $) {
+
+	var options = {
+		center : {
+			lat : 37.791350,
+			lng : -122.435883
+		},
+		zoom : 10
+	};
+
+	var maps = new google.maps.Map($("#map-canvas")[0], options);
+}(window, google, jQuery));
