9a89faba65a4fc24c5d9a8acff1bf33d6d37e368
unknown <kotas01@SCL-KotaS01.corp.pvt>
2015-05-21 17:12:49 -0700

    using the Mapster Module to create the Maps.

--- main/webapp/resources/mytheme/js/home.js
+++ main/webapp/resources/mytheme/js/home.js
@@ -1,6 +1,5 @@
 (function(window, google, $, mapster) {
 
-	var options = mapster.MAP_OPTIONS;
-
-	var maps = new google.maps.Map($("#map-canvas")[0], options);
+	var maps= mapster.create($("#map-canvas")[0], mapster.MAP_OPTIONS);
+	maps.zoom(16);
 }(window, google, jQuery,window.Mapster||(window.Mapster={})));
