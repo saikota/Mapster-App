00c1efd3bf0c458faff109e334ada230b479e690
Sai Kota <sai.kota@hotmail.com>
2015-05-25 13:06:58 -0700

    Adding a new marker to page. & checking remove functionality

--- main/webapp/resources/mytheme/js/home.js
+++ main/webapp/resources/mytheme/js/home.js
@@ -1,20 +1,24 @@
 (function(window, google, $, mapster) {
 
-	var maps= mapster.create($("#map-canvas")[0], mapster.MAP_OPTIONS);
-	maps.zoom(16);
-	var marker=maps.addMarker({
+	var map= mapster.create($("#map-canvas")[0], mapster.MAP_OPTIONS);
+//	map.zoom(16);
+	var marker=map.addMarker({
 		lat : 37.791350,
 		lng : -122.435883,
 		draggable : true,
 		visible:true,
-		content:' i like good food',
-//		event:{
-//			name:'click'
-////			callback:function(){
-////				alert(' i am dragged just now');
-////			}
-//		}
+		content:' i like good food'
 		});
-	maps._removeMarker(marker);
 	
+	var marker2 = map.addMarker({
+	    id: 2,
+	    lat: 37.781350,
+	    lng: -122.485883,
+	    content: 'I like rice'
+	   }); 
+	
+	console.log(map.removeBy(function(marker) {
+	    return marker.id === 2;
+	  })); 
+	  
 }(window, google, jQuery,window.Mapster||(window.Mapster={})));
