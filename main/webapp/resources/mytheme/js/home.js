30b92cd21e49f1fcb5205387ccc7db1c787bb602
unknown <kotas01@SCL-KotaS01.corp.pvt>
2015-05-25 01:16:48 -0700

    cleaned up code and using the new event api to call custom callback functions.

--- main/webapp/resources/mytheme/js/home.js
+++ main/webapp/resources/mytheme/js/home.js
@@ -2,18 +2,17 @@
 
 	var maps= mapster.create($("#map-canvas")[0], mapster.MAP_OPTIONS);
 	maps.zoom(16);
-	maps._on('click',function(e){
-		console.log("you have clicked on this Map",this);
-		
-	});
-	maps._on('dragend',function(){
 
-		console.log(" you are done dragging", this);
-	})
 	maps.addMarker({
 		lat : 37.791350,
 		lng : -122.435883,
 		draggable : true,
-		visible:true
+		visible:true,
+		event:{
+			name:'dragend',
+			callback:function(){
+				alert(' i am dragged just now');
+			}
+		}
 		});
 }(window, google, jQuery,window.Mapster||(window.Mapster={})));
